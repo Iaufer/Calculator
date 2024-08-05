58dc8e404143ae9bb7e44c8a33792d40f01c3e50
Artyom <lauferartyom@vk.com>
2024-08-05 13:56:50 +0500

    Added variables for input elements and result button. Added drop-down list for selecting operation in calculator

---
+++ index.js
@@ -0,0 +1,6 @@
+const inputANode = document.querySelector('.js-input-a');
+const inputBNode = document.querySelector('.js-input-b');
+const btnResultNode = document.querySelector('.js-btn-result');
+const outputNode = document.querySelector('.js-output');
+
+
