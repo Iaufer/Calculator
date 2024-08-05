d34d16ae149eb161e447aec61aed800b6fb07f9a
Artyom <lauferartyom@vk.com>
2024-08-05 14:01:39 +0500

    Added result button handler and calculation logic with selected operation

--- index.js
+++ index.js
@@ -3,4 +3,19 @@ const inputBNode = document.querySelector('.js-input-b');
 const btnResultNode = document.querySelector('.js-btn-result');
 const outputNode = document.querySelector('.js-output');
 
+const selectOperationNode = document.querySelector('.js-select-operation');
+
+
+btnResultNode.addEventListener('click', function(){
+    const a = Number(inputANode.value);
+    const b = Number(inputBNode.value);
+    const operation = Number(selectOperationNode.value)
+
+    const result = calc({
+        a, b, operation
+    })
+
+    console.log(result)
+});
+
 
