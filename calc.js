ace8be1d4e82fbb0babe9ddc57b8b6ce315c67db
Artyom <lauferartyom@vk.com>
2024-08-05 13:38:55 +0500

    rename main to math, create calc file and simple logic written, include math and calc in index.html

---
+++ calc.js
@@ -0,0 +1,24 @@
+const OPERATION = {
+    sum: '+',
+    subs: '-',
+    mul: '*',
+    div: '/'
+}
+
+
+function calc({a, b, operation}){
+    let result = null;
+
+    switch (operation){
+        case OPERATION.sum:
+            break
+        case OPERATION.subs:
+            break
+        case OPERATION.mul:
+            break
+        case OPERATION.div:
+            break
+    }
+
+    return result;
+}
